499e5186c19a189f96ba3dc090509432a5f5e50a
Alex Zilman <zilman.alex@gmail.com>
2026-09-14 11:50:58 +0300

    Use SVG favicon with dark mode support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,14 +25,8 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      {
-        url: "/favicon.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/favicon-dark.png",
-        media: "(prefers-color-scheme: dark)",
-      },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.png", type: "image/png" },
     ],
     apple: "/apple-touch-icon.png",
   },
